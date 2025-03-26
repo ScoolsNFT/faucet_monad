35c85be490d47eaa07b3a8391496417b30eed28a
ScoolsNFT <dampunisher@gmail.com>
2025-03-26 21:13:32 +0100

    Updated faucet logic and added fireworks on claim

--- faucet.js
+++ faucet.js
@@ -141,29 +141,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Faucet is live at http://localhost:${PORT}`);
 });
-javascript
-Copier
-Modifier
-const axios = require('axios');
-
-const faucetUrl = "https://scools-faucet-monad.onrender.com/";
-const address = "0x39DF80AD33A7FD0bEc56E41a101F8Dde4023654F";
-
-if (!ethers.isAddress(address)) {
-    console.error("L'adresse fournie est invalide");
-    process.exit(1);
-}
-
-axios.post(faucetUrl, { address })
-  .then(response => {
-    console.log("Faucet Response:", response.data);
-  })
-  .catch(error => {
-    if (error.response) {
-      console.error("Server Response Error:", error.response.data);
-    } else if (error.request) {
-      console.error("No Response from Server:", error.request);
-    } else {
-      console.error("Request Error:", error.message);
-    }
-  });
